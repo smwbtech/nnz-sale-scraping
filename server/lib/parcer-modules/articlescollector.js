d4cc37e85e7f447f543c6ea9b2df934dbc1bd360
Роман Привлов <romanprivlov@MacBook-Pro-Roman.local>
2018-02-14 23:27:53 +0300

    test parser - done

--- server/lib/parcer-modules/articlescollector.js
+++ server/lib/parcer-modules/articlescollector.js
@@ -5,11 +5,12 @@ const config = require('./../config');
 function collect(link) {
     return (async () => {
 
-        const browser = await puppeteer.launch({headless: false});
+        const browser = await puppeteer.launch({headless: config.headless});
         const page = await browser.newPage();
 
         //Основные селекторы nnz
         const nnz = {
+            productNameBlock: '.prev-product_l .prev-product_name',
             articleBlock: '.prev-product_l .prev-product_art',
             buttonMore: '.btn_add.show_catalog'
         };
@@ -27,18 +28,22 @@ function collect(link) {
         // Получаем артикулы, которые загрузились сразу
         console.log('we are here');
 
-        var articles = await page.evaluate( async (articleSelector, buttonSelector) => {
+        var articles = await page.evaluate( async (articleSelector, buttonSelector, nameSelector) => {
 
                 let result = await new Promise( (resolve, rejected) => {
                     let intervalId = setInterval( () => {
-                        if(document.querySelector('.btn_add.show_catalog') !== null) {
-                            document.querySelector('.btn_add.show_catalog').click();
+                        if(document.querySelector(buttonSelector) !== null) {
+                            document.querySelector(buttonSelector).click();
                         }
                         else {
                             let res = [];
-                            let articles = document.querySelectorAll('.prev-product_l .prev-product_art');
+                            let names = document.querySelectorAll(nameSelector)
+                            let articles = document.querySelectorAll(articleSelector);
                             for(let i = 0; i < articles.length; i++) {
-                                res.push(articles[i].innerText.slice(9));
+                                res.push({
+                                    article: articles[i].innerText.slice(9),
+                                    name: names[i].innerText
+                                });
                             }
                             console.log(res);
                             resolve(res);
@@ -48,7 +53,7 @@ function collect(link) {
                 });
                 return result;
 
-        }, nnz.articleBlock, nnz.buttonMore);
+        }, nnz.articleBlock, nnz.buttonMore, nnz.productNameBlock);
 
 
         console.log(articles);
--- server/lib/parcer-modules/parser.js
+++ server/lib/parcer-modules/parser.js
@@ -2,7 +2,10 @@ const puppeteer = require('puppeteer');
 const ProgressBar = require('progress');
 const config = require('./../config');
 
-function parseFeatures(article, schema) {
+function parseFeatures(articles, schema) {
+
+    var articles = articles.slice(0, 15);
+
     return (async () => {
 
         const browser = await puppeteer.launch({headless: config.headless});
@@ -31,65 +34,75 @@ function parseFeatures(article, schema) {
         let device = null;
         let parsedObj = null;
 
-        console.log(pattern);
+        for(let i = 0; i < articles.length; i++) {
 
+            //NOTE: Этот блок актуален, если у нас есть только артикулы
 
-        try {
-            await page.goto('http://new.nnz-ipc.ru/', {timeout: 30000});
-            await page.type(nnz.searchInput, article);
-            await page.click(nnz.searchSubmit);
-            await page.waitForSelector(nnz.searchRes, {timeout: 30000});
+            // try {
+            //     await page.goto('http://new.nnz-ipc.ru/', {timeout: 30000});
+            //     await page.type(nnz.searchInput, articles[i]);
+            //     await page.click(nnz.searchSubmit);
+            //     await page.waitForSelector(nnz.searchRes, {timeout: 30000});
+            //
+            //     device = await page.evaluate(linkSelector => {
+            //         let element = document.querySelector(linkSelector);
+            //         return {
+            //             name: element.innerHTML,
+            //             link: 'http://new.nnz-ipc.ru/' + element.getAttribute('href')
+            //         };
+            //     }, nnz.searchRes);
+            // }
+            //
+            // catch(err) {
+            //     console.error(err);
+            //     continue;
+            // }
 
-            device = await page.evaluate(linkSelector => {
-                let element = document.querySelector(linkSelector);
-                return {
-                    name: element.innerHTML,
-                    link: 'http://new.nnz-ipc.ru/' + element.getAttribute('href')
-                };
-            }, nnz.searchRes);
-        }
+            try {
+                await page.goto(site.link);
+                await page.type(site.searchInput, articles[i].name);
+                await page.focus(site.searchInput);
+                await page.keyboard.down('Enter');
+                await page.waitForSelector(site.searchRes, {timeout: 15000});
+            }
+            catch(err) {
+                console.log(err);
+                articles[i].data = null
+                continue;
+            }
+            try {
 
-        catch(err) {
-            console.error(err);
-        }
+                await page.click(site.searchRes);
+                await page.waitForSelector(site.featureBlock);
 
-        console.log(device);
-
-        try {
-            await page.goto(site.link);
-            await page.type(site.searchInput, device.name);
-            await page.click(site.searchSubmit);
-            await page.waitForSelector(site.searchRes);
-            await page.click(site.searchRes);
-            await page.waitForSelector(site.featureBlock);
-
-            parsedObj = await page.evaluate( (selector, patternObj) => {
-                let elements = document.querySelectorAll(selector);
-                let pattern = patternObj;
-                let res = {};
-                for(let prop in pattern) {
-                    let regExp = new RegExp( pattern[prop], 'i');
-                    for(let i = 0; i < elements.length; i++) {
-                        if(regExp.test(elements[i].innerText)) {
-                            res[prop] = elements[i].nextElementSibling.innerText;
+                articles[i].data = await page.evaluate( (selector, patternObj) => {
+                    let elements = document.querySelectorAll(selector);
+                    let pattern = patternObj;
+                    let res = {};
+                    for(let prop in pattern) {
+                        let regExp = new RegExp( pattern[prop], 'i');
+                        for(let i = 0; i < elements.length; i++) {
+                            if(regExp.test(elements[i].innerText)) {
+                                res[prop] = elements[i].nextElementSibling.innerText;
+                            }
                         }
                     }
-                }
-                return res;
-            }, site.featureName, pattern)
+                    return res;
+                }, site.featureName, pattern)
 
+            }
 
-
-        } catch (e) {
-            console.log(e);
+            catch (err) {
+                articles[i].data = null
+                console.log(err);
+                continue;
+            }
         }
 
-
-        console.log(device);
         console.log(parsedObj);
         await browser.close();
 
-        return parsedObj;
+        return articles;
 
     })();
 }
