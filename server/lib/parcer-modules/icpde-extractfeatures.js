0add2d318440bc2d7c63a9fe657e8de5b4c23f1a
smwbtech <smwb.tech@gmail.com>
2018-02-16 17:28:40 +0300

    Working on field translation from searchResult into nnz format

---
+++ server/lib/parcer-modules/icpde-extractfeatures.js
@@ -0,0 +1,20 @@
+
+function extractFeatures(searchResult) {
+
+    const extractedFeatures = {
+
+        'Звуковой контроллер' : searchResult['Звуковой контроллер'],
+        'Видеоконтроллер': searchResult['Видеоконтроллер'],
+        'Интерфейсы': searchResult['Интерфейсы'],
+        'Поддержка IRIS': searchResult['searchResult'],
+        'LPT': '',
+        'RS-232': '',
+        'RS-232/422/485': '',
+        'RS-422/485': '',
+        
+
+    }
+
+}
+
+//Helpers
